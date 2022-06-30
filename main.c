430eda174b89bb1ab0e7f0e9e87994f3f18036f4
Bohdan Zubko <108411272+bohdanzubko@users.noreply.github.com>
2022-06-30 17:04:21 +0300

    Update main.c
    Added the ability to continue working with the program and enter a new number

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /**
 * @file Practice_1
-* @author Зубко Б.Р., гр. 515, варіант 25
+* @author Зубко Б.Р., гр. 515, варіант 27
 * @date 28 червня 2022
 * @brief Практика
 *
@@ -13,7 +13,9 @@ int main() {
 	setlocale(LC_CTYPE, "Ukr"); //зміна кодової таблиці символів
 	int arr[10] = { 0 }; //масив окремих цифр
 	long long int input_digit;
-	repeat: printf("Введiть число: "); //запрошення до вводу
+	int k;
+	while (true) {
+	repeat: printf("\nВведiть число: "); //запрошення до вводу
 	scanf_s("%lld", &input_digit); //ввід
 	if (input_digit < INT_MIN || input_digit > INT_MAX) { //перевірка на діапазон введеного числа, що відвповідає діапазону типу __int32
 		printf("Невiрний ввiд!\n"); //повідомлення
@@ -23,5 +25,12 @@ int main() {
 	digits_arr(digit, arr); //сворення масиву з окремих цифр
 	printf("%d -> ", digit); //вивід
 	in_words(digit, arr); //виклик функції виводу введеного числа словами
-	return 0; //завершення
+	printf("\n\nЧи бажаєте ви ввести нове число?\n 1 - так\n 0 - нi\nВибiр: ");
+	scanf_s("%d", &k);
+	if (!k) {
+		printf("\nЗавершення...\n");
+		goto end;
+	}
+	}
+	end: return 0; //завершення
 }
